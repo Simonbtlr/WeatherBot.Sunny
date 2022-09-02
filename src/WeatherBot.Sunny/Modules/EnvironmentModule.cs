de2c3712874c5c0c11fd069f155b315205a30a43
Arthur <54881795+Simonbtlr@users.noreply.github.com>
2022-09-02 23:38:34 +0300

    Release/1.1.0 (#6)
    * [WBS-2] Add runtime config for staging (#3)
    
    * [WBS-4] Setup workflows (#5)

--- src/WeatherBot.Sunny/Modules/EnvironmentModule.cs
+++ src/WeatherBot.Sunny/Modules/EnvironmentModule.cs
@@ -5,16 +5,6 @@ namespace WeatherBot.Sunny.Modules;
 
 public static class EnvironmentModule
 {
-    public static void LoadSecrets(this IHostEnvironment env)
-    {
-        var paths = new[]
-        {
-            ".env",
-            env.IsProduction()
-                ? ".env.production"
-                : ".env.staging"
-        };
-
-        Env.LoadMulti(paths);
-    }
+    public static void LoadSecrets(this IHostEnvironment env) =>
+        Env.Load();
 }
