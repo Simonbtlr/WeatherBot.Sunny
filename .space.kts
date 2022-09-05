f20b9d82731f3872ed2e86987113da4c8ba51588
Simon Butler <SimonGamezs@gmail.com>
2022-09-05 22:34:57 +0000

    edit .space.kts

--- .space.kts
+++ .space.kts
@@ -1,25 +1,3 @@
-job("Build and push Docker") {
-    startOn {
-    	gitPush { 
-        	branchFilter {
-            	"release/**"
-            }
-        }
-    }
-    
-    docker {
-        build {
-            context = "docker"
-            file = "./dockerfile"
-            labels["vendor"] = "pinguin"
-        }
-
-        push("pinguin.registry.jetbrains.space/p/mp/mydocker/myimage") {
-            tags("version")
-        }
-    }
-}
-
 container("openjdk:11") {
     kotlinScript { api ->
         api.space().projects.automation.deployments.start(
