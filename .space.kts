b542280a573432275053b40b57b79a4f0e3319b0
Simon Butler <SimonGamezs@gmail.com>
2022-09-05 22:33:22 +0000

    Edit .space.ktl

--- .space.kts
+++ .space.kts
@@ -19,3 +19,15 @@ job("Build and push Docker") {
         }
     }
 }
+
+container("openjdk:11") {
+    kotlinScript { api ->
+        api.space().projects.automation.deployments.start(
+            project = api.projectIdentifier(),
+            targetIdentifier = TargetIdentifier.Key("staging"),
+            version = "1.0.0",
+            // automatically update deployment status based on a status of a job
+            syncWithAutomationJob = true
+        )
+    }
+}
