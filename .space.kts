ac454e9bebd98e51fe42f05f13f01a982132c2a1
Simon Butler <SimonGamezs@gmail.com>
2022-09-05 22:37:46 +0000

    edit .space.kts

--- .space.kts
+++ .space.kts
@@ -1,11 +1,13 @@
-container("openjdk:11") {
-    kotlinScript { api ->
-        api.space().projects.automation.deployments.start(
-            project = api.projectIdentifier(),
-            targetIdentifier = TargetIdentifier.Key("staging"),
-            version = "1.0.0",
-            // automatically update deployment status based on a status of a job
-            syncWithAutomationJob = true
-        )
-    }
+job("Deploy") {
+	container("openjdk:11") {
+    	kotlinScript { api ->
+        	api.space().projects.automation.deployments.start(
+            	project = api.projectIdentifier(),
+            	targetIdentifier = TargetIdentifier.Key("staging"),
+            	version = "1.0.0",
+            	// automatically update deployment status based on a status of a job
+            	syncWithAutomationJob = true
+        	)
+    	}
+	}
 }
